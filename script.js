2bb7a7e222f5148cee8f10debccb918550f4b264
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-20 19:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Function declaration
+function functionName() {
+  alert("functionName");
+}
+
+// Example of a function expression
+const AccioJob = function() {
+  alert("AccioJob");
+};
+
+function printFunctionName() {
+  // Print the name of the function directly
+  functionName(); // Alerts: "functionName"
+  
+  // Or use a function expression
+  AccioJob(); // Alerts: "AccioJob"
+}
